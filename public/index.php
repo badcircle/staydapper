3b1777ad3bf5cb3b0b654f0587db7baeb48b324d
Adam Wood <wood@badcircle.com>
2021-11-23 22:17:20 -0600

    initial commit

---
+++ public/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Stay Dapper Beard Co.</title>
+    <link rel="stylesheet" href="./css/uikit.min.css">
+    <script src="./js/uikit.min.js"></script>
+    <script src="./js/uikit-icons.min.js"></script>
+</head>
+
+<body>
+
+<div class="uk-section uk-section-primary" id="welcome">
+  <div class="uk-container-expand">
+    <div uk-grid="" class="uk-child-width-1-3 uk-flex-bottom">
+      <div></div>
+      <div></div>
+      <div class="uk-position-relative"><img src="./img/logo_large.png" alt="" class="floater"></div>
+    </div>
+  </div>
+</div>
+
+<div class="uk-section">
+  <div class="uk-container">
+    <div uk-grid="">
+      <div class="uk-width-1-2">Test</div>
+      <div class="uk-width-1-2">Test</div>
+    </div>    
+  </div>
+</div>
+
+<style>
+#welcome {
+  background-image: url('./img/grit-bg.png');
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+}
+.floater {
+  transform: translateY(25%);
+  max-width: 100%;
+}
+</style>
+</body>
+
+</html>
